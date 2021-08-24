5f30b9dcaf61343048640e723049231c0523796b
Jose Erney Ospina <joseerneyosp@gmail.com>
2021-08-23 22:31:00 -0500

    Refactor( shapes ): change of no dinamic data to functions

--- shapes.js
+++ shapes.js
@@ -6,11 +6,23 @@ console.group('---- SQUARE ----');
 const squareSide = 5;
 console.log(`Los lados del cuadrado miden : ${squareSide} cm`)
 
-const squarePerimeter = squareSide * 4;
-console.log(`El perimetro del cuadrado : ${squarePerimeter} cm`);
+const squarePerimeter = ( side ) => {
+    return side * 4;
+}
 
-const squareArea = squareSide * squareSide;
-console.log(`El area del cuadrado : ${squareArea} cm^2`);
+squarePerimeter( 2 );
+
+// const squarePerimeter = squareSide * 4;
+// console.log(`El perimetro del cuadrado : ${squarePerimeter} cm`);
+
+const squareArea = ( side ) => {
+    return side * side;
+}
+
+squareArea( 4 );
+
+// const squareArea = squareSide * squareSide;
+// console.log(`El area del cuadrado : ${squareArea} cm^2`);
 
 console.groupEnd();
 
@@ -19,23 +31,35 @@ console.groupEnd();
 // Triangle code
 console.group('---- TRIANGLE ----');
 
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-const triangleHeight = 5.5;
+// const triangleSide1 = 6;
+// const triangleSide2 = 6;
+// const triangleBase = 4;
+// const triangleHeight = 5.5;
+//
+// console.log(`Los lados del cuadrado miden :
+//     ${triangleSide1} cm
+//     ${triangleSide2} cm
+//     ${triangleBase} cm`);
+//
+// console.log(`La altura del triangulo es de : ${triangleHeight} cm`);
 
-console.log(`Los lados del cuadrado miden :
-    ${triangleSide1} cm
-    ${triangleSide2} cm
-    ${triangleBase} cm`);
+// const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
+// console.log(`El perimetro del triangulo es : ${trianglePerimeter} cm`);
 
-console.log(`La altura del triangulo es de : ${triangleHeight} cm`);
+const trianglePerimeter = ( side1, side2, base ) => {
+    return side1 + side2 + base;
+}
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-console.log(`El perimetro del triangulo es : ${trianglePerimeter} cm`);
+trianglePerimeter(2, 4, 6);
 
-const triangleArea = (triangleBase * triangleHeight) / 2;
-console.log(`El area del triangulo es : ${triangleArea} cm^2`);
+// const triangleArea = (triangleBase * triangleHeight) / 2;
+// console.log(`El area del triangulo es : ${triangleArea} cm^2`);
+
+const triangleArea = ( base, height ) => {
+    return ( base * height ) / 2;
+}
+
+triangleArea( 2, 7 );
 
 console.groupEnd();
 
@@ -44,21 +68,34 @@ console.groupEnd();
 // Circle code
 console.group('---- CIRCLE ----');
 
-const circleRadio = 4;
-const circleDiameter = circleRadio * 2;
+// const circleRadio = 4;
+// const circleDiameter = circleRadio * 2;
 const PI = Math.PI;
-const circlePerimeter = PI;
+// const circlePerimeter = PI;
 
-pCirculo = circleDiameter * PI;
+const circleDiameter = ( radio ) => {
+    return radio * 2;
+}
+
+const circlePerimeter = ( radio ) => {
+    const diameter = circleDiameter( radio );
+    return diameter * PI;
+}
+
+// circlePerimeter = circleDiameter * PI;
 // Circumference
-const circleCircumference = pCirculo;
+// const circleCircumference = pCirculo;
 
 // Area
-const circleArea = ( circleRadio * circleRadio ) * PI;
+// const circleArea = ( circleRadio * circleRadio ) * PI;
 
-console.log(`El radio del circulo es : ${circleRadio} cm`);
-console.log(`El diametro del circulo es : ${circleDiameter} cm`);
-console.log(`El perimetro del circulo es : ${pCirculo} cm`);
-console.log(`El area del circulo es : ${circleArea} cm^2`);
+const circleArea = ( radio ) => {
+    return (radio * radio ) * PI;
+}
+
+// console.log(`El radio del circulo es : ${circleRadio} cm`);
+// console.log(`El diametro del circulo es : ${circleDiameter} cm`);
+// console.log(`El perimetro del circulo es : ${pCirculo} cm`);
+// console.log(`El area del circulo es : ${circleArea} cm^2`);
 
 console.groupEnd();
